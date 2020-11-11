29ffa83d9ea91ae314e96e64b3132bf2d7b09307
Antoine <aberthier15@gmail.com>
2020-11-11 19:36:15 +0100

    fix

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,3 +1,5 @@
 export {Presentation} from "./Presentation";
 export {Experiances} from "./Experiances";
 export {Skills} from "./Skills";
+export {Hobby} from "./Hobby";
+export {Formation} from "./Formation";
